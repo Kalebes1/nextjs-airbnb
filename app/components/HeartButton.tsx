041891a9ad3378da407b145ec64bba25dfca45a6
Kalebe <craftanosclip74@gmail.com>
2023-11-23 18:14:13 -0300

    Favoriting Functionality

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,5 +1,6 @@
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorite";
 
 interface HeartButtonProps{
     listingId: string;
@@ -10,8 +11,10 @@ const HeartButton: React.FC<HeartButtonProps> = ({
     listingId,
     currentUser
 }) => {
-    const hasFavorited = false;
-    const toggleFavorite = () => {};
+    const { hasFavorited, toggleFavorite } = useFavorite({
+        listingId,
+        currentUser
+    })
   return (
     <div onClick={toggleFavorite} className="relative hover:opacity-80 transition cursor-pointer">
         <AiOutlineHeart
